84bc00aed261fcf714c4bf6a82df33a168c0b464
AR-Shahin <mdshahinmije96@gmail.com>
2022-03-06 23:08:32 +0600

    java user input

--- Theory/Basic/First.java
+++ Theory/Basic/First.java
@@ -1,7 +1,18 @@
+import java.util.*;
 
 public class First {
+
+    public void sayHello(String name) {
+        System.out.println("Hello " + name);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        First self = new First();
+        Scanner sc = new Scanner(System.in);
+        String name;
+        name = sc.nextLine();
+        self.sayHello(name);
+
     }
 }
 
