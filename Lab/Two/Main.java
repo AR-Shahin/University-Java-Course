f89c842e5dd62f512004edf0b1f710b456b55a67
AR-Shahin <mdshahinmije96@gmail.com>
2022-03-16 22:22:58 +0600

    string problem solving

--- Lab/Two/Main.java
+++ Lab/Two/Main.java
@@ -2,6 +2,7 @@
 import java.util.Scanner;
 
 import classes.MathematicalOperations;
+import classes.StringOperation;
 import classes.Hepler.Helper;
 
 public class Main {
@@ -11,20 +12,32 @@ public class Main {
         Main self = new Main();
         Helper help = new Helper();
         sc = new Scanner(System.in);
-        System.out.println("Enter Array Size : ");
-        int size = sc.nextInt();
-        double arr[] = new double[size];
-        for (int i = 0; i < size; i++) {
-            System.out.println("Value " + (i + 1) + " : ");
-            arr[i] = sc.nextDouble();
-        }
+        // System.out.println("Enter Array Size : ");
+        // int size = sc.nextInt();
+        // double arr[] = new double[size];
+        // for (int i = 0; i < size; i++) {
+        // System.out.println("Value " + (i + 1) + " : ");
+        // arr[i] = sc.nextDouble();
+        // }
         // printValues(arr);
-        MathematicalOperations math = new MathematicalOperations(arr);
-        help.print("Sum of Array : " + math.addition());
-        help.print("Multiplication of Array : " + math.multiplication());
-        help.print("Maximum Value of Array : " + math.getMaxValue());
-        help.print("Minimum Value, of Array : " + math.getMinValue());
-        math.powerOfEachElements(3);
+        // MathematicalOperations math = new MathematicalOperations(arr);
+        // help.print("Sum of Array : " + math.addition());
+        // help.print("Multiplication of Array : " + math.multiplication());
+        // help.print("Maximum Value of Array : " + math.getMaxValue());
+        // help.print("Minimum Value, of Array : " + math.getMinValue());
+        // math.powerOfEachElements(3);
+
+        StringOperation str = new StringOperation();
+        str.setString("ars");
+        // help.print(str.reverseString());
+        // str.checkPalindromeOrNot();
+        String names[] = new String[5];
+        names[0] = "Shahin";
+        names[1] = "Asik";
+        names[2] = "Tanzim";
+        names[3] = "Nipa";
+        names[4] = "Kaberi";
+        str.concatenate(names);
     }
 
     void inputArrayValues(int size) {
--- Lab/Two/classes/StringOperation.java
+++ Lab/Two/classes/StringOperation.java
@@ -1,10 +1,68 @@
 package classes;
 
-public class StringOperation {
+import classes.Hepler.Helper;
+
+public class StringOperation extends Helper {
     private String str;
 
-    public String reverseString() {
-
-        return str;
+    public void setString(String str) {
+        this.str = str;
     }
+
+    public String reverseString() {
+        String revStr = "";
+        char ch;
+        for (int i = 0; i < this.str.length(); i++) {
+            ch = str.charAt(i);
+            revStr = ch + revStr;
+        }
+        return revStr;
+    }
+
+    public void checkPalindromeOrNot() {
+
+        String str1 = this.str.toLowerCase();
+        String str2 = this.reverseString().toLowerCase();
+
+        System.out.println(str1.equals(str2) ? "Palindrome!" : "Not Palindrome!");
+    }
+
+    public void checkSubStringOrNot(String substr) {
+
+        System.out.println(this.str.toLowerCase().contains(substr.toLowerCase()) ? "Yes!" : "No!");
+
+    }
+
+    public void checkSubStringOrNot(String data[], String substr) {
+
+        // System.out.println(data[0]);
+        String temp[] = new String[data.length];
+        for (int i = 0; i < data.length; i++) {
+            if (data[i].toLowerCase().contains(substr.toLowerCase())) {
+                temp[i] = data[i];
+                print(temp[i]);
+            }
+
+            // System.out.println(data[i].toLowerCase().contains(substr.toLowerCase()) ?
+            // "Yes!" : "No!");
+
+            // System.out.println(data[i].contains("s"));
+        }
+        // System.out.println("\n--------------");
+        // for (int i = 0; i < temp.length; i++) {
+        // System.out.print(temp[i] + " ");
+        // }
+
+    }
+
+    public void concatenate(String str[]) {
+        String sentence = "";
+
+        for (int i = 0; i < str.length; i++) {
+            sentence += (str[i] + " ");
+        }
+
+        print(sentence);
+    }
+
 }
