2b8e78f784bd8a2d5b6a2f044d2d6104b7e15eff
AR-Shahin <mdshahinmije96@gmail.com>
2022-03-30 23:23:54 +0600

    third class

--- Course/src/shahin/ar/Main.java
+++ Course/src/shahin/ar/Main.java
@@ -13,18 +13,29 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
         int a ,b;
-        a = sc.nextInt();
-        b = sc.nextInt();
+//        double x = 1.25;
+//        int y = (int) x;
+//        int z = 10;
+//        double q = (double)z;
+//        System.out.println("X " + x);
+//        System.out.println("Y " + y);
+//        System.out.println("Z " + z);
+//        System.out.println("Q " + q);
 
+//        a = 10;
+//        a = a + 5;
+//        a += 5;
+//        char ch = 'c';
+//        System.out.println("A " + a);
 
-        MathematicalOperation math1 = new MathematicalOperation(a,b);
+//        System.out.println("Pre Increment " + ++a); // 11
+//        System.out.println("Post Increment " + a++); //
+//        System.out.println("Post Increment " + a++); //
+//        System.out.println("Post Increment After " + a);
 
-        System.out.println(math1.add());
-        System.out.println(math1.sub());
-        System.out.println(math1.multify());
-        System.out.println(math1.div());
-
-
+//        System.out.println("Pre Decrement  " + --a);
+//        System.out.println("Post Decrement  " + a--);
+//        System.out.println("Post Decrement After " + a);
 //        System.out.println("Main Block!");
 //       System.out.println(temp);
 
@@ -39,9 +50,31 @@ public class Main {
 //        System.out.println(Person.y);
 //        System.out.println(p2.y);
 
+        String str = "shahin";
+
+//        System.out.println(str);
+//        System.out.println(str.length());
+//        System.out.println(str.toUpperCase());
+//        System.out.println(str.toLowerCase());
+//        System.out.println(str.contains("z"));
+//        System.out.println(str.concat(" Bhai"));
+
+    a = 10;
+    b = 50;
+
+//    if(a == b){
+//        System.out.println("True");
+//    }else{
+//        System.out.println("False");
+//    }
+
+
+//    System.out.println("sdd" + a);
+//    System.out.println(a == b ? "True" : "False");
+
     }
 }
-
+// 2/3+5/15-8/63+11/255+14/1023-17/4095
  class Person{
     int x;
     static int y = 100;
