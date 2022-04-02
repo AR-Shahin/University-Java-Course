ecf854c6c5b09d609ff27be0bb50862e6c398b7d
AR-Shahin <mdshahinmije96@gmail.com>
2022-04-02 23:30:00 +0600

    evn odd

--- Course/src/shahin/ar/Main.java
+++ Course/src/shahin/ar/Main.java
@@ -10,17 +10,30 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        float base,height;
-        double area;
-        base = sc.nextFloat();
-        height = sc.nextFloat();
+        int id,evn = 0,odd = 0;
 
-        area = 0.5 * base * height;
+       id = sc.nextInt();
 
+        while (id != 0){ // 12345
+            int rem = id % 10; // 5
 
-        System.out.println("Area : " + area);
+            if(rem % 2 == 0){ // 5 % 2 ==
+                evn ++;
+            }else {
+                odd++;
+            }
+            id = id / 10; // 12345 / 10
+        }
 
-
+        if(evn > odd){
+            System.out.println("Even Win");
+        }
+        if(evn < odd){
+            System.out.println("ODD Win");
+        }
+        if(evn == odd){
+            System.out.println("Tie");
+        }
     }
 }
 
