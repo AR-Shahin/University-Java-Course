cf010793184ee301cb762b85485d95b9fa4ebf8a
AR-Shahin <mdshahinmije96@gmail.com>
2022-04-18 22:36:58 +0600

    triangle

--- Course/src/shahin/ar/Main.java
+++ Course/src/shahin/ar/Main.java
@@ -12,14 +12,8 @@ public class Main {
         int n = 4;
 
         for(int row = 1; row <=n; row++){
-            for(int col = 1; col <=n; col++){
-                if(row==1|| row == n|| col==1|| col == n)
-                {
-                    System.out.print("*");
-                }
-                else{
-                    System.out.print(" ");
-            }
+            for(int col = 1; col <=row; col++){
+                System.out.print("* ");
             }
             System.out.print("\n");
         }
