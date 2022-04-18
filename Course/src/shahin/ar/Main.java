3f1b60614b9f805dec306190853e6121c55f3f34
AR-Shahin <mdshahinmije96@gmail.com>
2022-04-18 22:31:33 +0600

    empty star

--- Course/src/shahin/ar/Main.java
+++ Course/src/shahin/ar/Main.java
@@ -13,7 +13,13 @@ public class Main {
 
         for(int row = 1; row <=n; row++){
             for(int col = 1; col <=n; col++){
-                System.out.print("* ");
+                if(row==1|| row == n|| col==1|| col == n)
+                {
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+            }
             }
             System.out.print("\n");
         }
