7695b2a8fc5cbb957ae27fd3c96d3dea39770495
ar-shahin <mdshahinmije96@gmail.com>
2022-05-09 11:10:15 +0600

    file handling

---
+++ Theory/File/src/Main.java
@@ -0,0 +1,12 @@
+import file.FileHandling;
+
+public class Main {
+    public static void main(String[] args) {
+        FileHandling file = new FileHandling();
+
+        String data = "10,20,8,40,50";
+        file.setDataIntoFile(data);
+        file.getDataFromFile();
+        file.checkAdult();
+    }
+}
