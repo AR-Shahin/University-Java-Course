29ef58448c691052609fad3a8029a34e2ebde0c6
ar-shahin <mdshahinmije96@gmail.com>
2022-05-27 10:14:12 +0600

    add sum function

--- Theory/File/src/Main.java
+++ Theory/File/src/Main.java
@@ -4,13 +4,16 @@ public class Main {
     public static void main(String[] args) {
         FileHandling file = new FileHandling();
 
-//        String data = "10,20,8,40,50";
-//        file.setDataIntoFile(data);
-//        file.getDataFromFile();
+        file.getDataFromFile();
 //        file.checkHowManyVowels();
-
-        file.fileRead();
-        file.findMinMaxAvg();
+        file.setDataIntoFile("Sssssss");
+////        String data = "10,20,8,40,50";
+////        file.setDataIntoFile(data);
+////        file.getDataFromFile();
+////        file.checkHowManyVowels();
+//
+//        file.fileRead();
+//        file.findMinMaxAvg();
 
     }
 }
--- Theory/File/src/file/FileHandling.java
+++ Theory/File/src/file/FileHandling.java
@@ -16,6 +16,7 @@ public class FileHandling {
     public void setDataIntoFile(String data){
         File file = new File("output.txt");
 
+
         if(!file.exists()){
             try{
                 file.createNewFile();
@@ -33,6 +34,7 @@ public class FileHandling {
         }
     }
 
+
     public void getDataFromFile(){
         File file = new File("input.txt");
 
@@ -41,8 +43,7 @@ public class FileHandling {
                 Scanner sc = new Scanner(file);
                 while (sc.hasNextLine()){
                     String data = sc.nextLine();
-//                    System.out.println(data);
-                    this.splitFileData(data,' ');
+                    this.getSum(data,';');
                 }
             }catch (Exception e){
                 System.out.println(e.getMessage());
@@ -50,6 +51,18 @@ public class FileHandling {
         }else{
             System.out.println("File Not Found!");
         }
+    }
+
+    public void getSum(String data, Character ch){
+        int sum = 0;
+        String  temp[] = null;
+        for (int i = 0;i< data.length();i++){
+            temp = data.split(String.valueOf(ch));
+        }
+        for (int i = 0;i<temp.length;i++){
+            sum += Integer.parseInt(temp[i]);
+        }
+       System.out.println(sum);
     }
 
     public void storeDataInArrayFromFile(int data){
@@ -70,21 +83,11 @@ public class FileHandling {
 
     }
     public void test(){
-//        System.out.println(this.stringDatabase.get(0));
+
         System.out.println(this.stringDatabase.size());
     }
 
-    public void checkAdult(){
-        for(int age : this.intDatabase){
-            if(age >= 18){
-                System.out.println("Adult!");
-            }else{
-                System.out.println("Not Adult!");
-            }
-        }
-    }
-
-    public void checkHowManyVowels() {
+    public int checkHowManyVowels() {
         int vowel = 0;
         for (String word : this.stringDatabase) {
             Character ch = word.charAt(0);
@@ -93,66 +96,8 @@ public class FileHandling {
             }
         }
 
-        System.out.println("Vowel is : " + vowel);
+        return vowel;
     }
 
-    public void fileRead(){
-        File file = new File("foo.txt");
-        int index = -1;
-        if(file.exists()){
-            try{
-                Scanner sc = new Scanner(file);
-                while (sc.hasNextLine()){
-                    String data = sc.nextLine();
-                    this.arr[++index] = Integer.parseInt(data);
-                }
-            }catch (Exception e){
-                System.out.println(e.getMessage());
-            }
-        }else{
-            System.out.println("File Not Found!");
-        }
-    }
 
-    public void findMinMaxAvg(){
-        int max = this.arr[0];
-        int min = this.arr[0];
-        int sum = 0;
-        for(int i = 0;i<this.max_size;i++){
-            if(max < this.arr[i]){
-                max = this.arr[i];
-            }
-
-            if(min > this.arr[i]){
-                min = this.arr[i];
-            }
-
-            sum += this.arr[i];
-        }
-
-        String format = "Max = " + max
-                + "\nMin = " + min
-               + "\nAvg = " + sum/(double)this.max_size +"\n";
-        this.storeInFile(format);
-    }
-
-    public void storeInFile(String data){
-        File file = new File("bar.txt");
-
-        if(!file.exists()){
-            try{
-                file.createNewFile();
-            }catch (Exception e){
-                System.out.println(e.getMessage());
-            }
-        }else {
-            try{
-                FileWriter fileWriter = new FileWriter("bar.txt",true);
-                fileWriter.write(data);
-                fileWriter.close();
-            }catch (Exception e){
-                System.out.println(e.getMessage());
-            }
-        }
-    }
 }
