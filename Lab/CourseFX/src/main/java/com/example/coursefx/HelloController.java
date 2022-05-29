a592b5975f0428256d7363f2f94cbd2007a56d7a
ar-shahin <mdshahinmije96@gmail.com>
2022-05-29 22:23:21 +0600

    show data in same page

--- Lab/CourseFX/src/main/java/com/example/coursefx/HelloController.java
+++ Lab/CourseFX/src/main/java/com/example/coursefx/HelloController.java
@@ -2,9 +2,15 @@ package com.example.coursefx;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
@@ -13,13 +19,35 @@ public class HelloController {
     private Label welcomeText;
 
     @FXML
+    private TextArea display;
+    @FXML
     private Label title;
 
     @FXML
-    private TextField nameField;
+    private TextField nameField,emailField,phoneField;
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+
+    @FXML
+    void handleSubmitButton(ActionEvent event) {
+        String name = nameField.getText();
+        String email = emailField.getText();
+        String phone = phoneField.getText();
+
+        String format = "Name : " + name + "\nEmail : " + email + "\nPhone : " + phone;
+        display.setText(format);
+
+        this.clearForm();
+        System.out.println(name + email + phone);
+    }
+
+    private void clearForm(){
+        nameField.setText("");
+        emailField.setText("");
+        phoneField.setText("");
     }
 
     @FXML
@@ -27,5 +55,19 @@ public class HelloController {
         title.setText("I am changed by clicking button!");
         String data = title.getText();
         System.out.println("Hello World");
+        try {
+            this.start();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void start() throws IOException {
+        Stage stage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
     }
 }
