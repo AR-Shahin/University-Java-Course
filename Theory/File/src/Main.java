61078142805cf09bfe9a441dd3cd6e56b42277ac
ar-shahin <mdshahinmije96@gmail.com>
2022-06-05 20:26:32 +0600

    final exam preparation

--- Theory/File/src/Main.java
+++ Theory/File/src/Main.java
@@ -1,19 +1,10 @@
+import file.DMSS;
 import file.FileHandling;
 
 public class Main {
     public static void main(String[] args) {
-        FileHandling file = new FileHandling();
-
-        file.getDataFromFile();
-//        file.checkHowManyVowels();
-        file.setDataIntoFile("Sssssss");
-////        String data = "10,20,8,40,50";
-////        file.setDataIntoFile(data);
-////        file.getDataFromFile();
-////        file.checkHowManyVowels();
-//
-//        file.fileRead();
-//        file.findMinMaxAvg();
+        DMSS file = new DMSS();
+        file.getDataFromFileForDate();
 
     }
 }
--- Theory/File/src/file/DMSS.java
+++ Theory/File/src/file/DMSS.java
@@ -1,8 +1,117 @@
 package file;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class DMSS {
 
-    public void countTotalLine(){
+    private int sum = 0;
+    private double mean_sum = 0;
+    private double mean_square_sum = 0;
+    private double mean = 0;
+    private int item = 0;
+
+    protected ArrayList<String> stringDatabase = new ArrayList<>();
+    public void getDataFromFileMeanSumMod(){
+        File file = new File("input.txt");
+
+        if(file.exists()){
+            try{
+                Scanner sc = new Scanner(file);
+                while (sc.hasNextLine()){
+                    String data = sc.nextLine();
+                    double x = Double.parseDouble(data);
+                    this.mean_sum += x;
+                    this.mean_square_sum += (x*x);
+                    this.item ++;
+//                    int sum = this.getSum(data,';'); // for sum
+                }
+                this.mean = mean_sum/item;
+                double variant = (mean_square_sum/item) - (mean*mean);
+                System.out.println(mean);
+                System.out.println(variant);
+                System.out.println(Math.sqrt(variant));
+
+//                this.setDataIntoFile(String.valueOf(sum)); // for sum
+            }catch (Exception e){
+                System.out.println(e.getMessage());
+            }
+        }else{
+            System.out.println("File Not Found!");
+        }
+    }
+
+    public int getSum(String data, Character ch){
+        String  temp[] = null;
+        for (int i = 0;i< data.length();i++){
+            temp = data.split(String.valueOf(ch));
+        }
+        for (int i = 0;i<temp.length;i++){
+            int num = Integer.parseInt(temp[i]);
+            if(num % 2 != 0){
+                this.sum += num;
+            }
+        }
+        return sum;
+    }
+
+    public void setDataIntoFile(String data){
+        File file = new File("output.txt");
+
+
+        if(!file.exists()){
+            try{
+                file.createNewFile();
+            }catch (Exception e){
+                System.out.println(e.getMessage());
+            }
+        }else {
+            try{
+                FileWriter fileWriter = new FileWriter("output.txt",true);
+                fileWriter.write(data);
+                fileWriter.close();
+            }catch (Exception e){
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public void getDataFromFileForDate(){
+        File file = new File("input.txt");
+
+        if(file.exists()){
+            try{
+                Scanner sc = new Scanner(file);
+                while (sc.hasNextLine()){
+                    String data = sc.nextLine();
+                    this.splitFileDataForDate(data,' ');
+                }
+            }catch (Exception e){
+                System.out.println(e.getMessage());
+            }
+        }else{
+            System.out.println("File Not Found!");
+        }
+    }
+
+    public void splitFileDataForDate(String data, Character separator){
+        String temp[] = null;
+        for (int i = 0;i< data.length();i++){
+            temp = data.split(String.valueOf(separator));
+        }
+
+        String format = "";
+        for (int i = 0;i<temp.length;i++){
+            if(temp[i].contains("/") && (temp[i].length() == 8 ||temp[i].length() == 10)){
+//                System.out.println(temp[i]);
+                format += temp[i] + "\n";
+            }
+        }
+
+        setDataIntoFile(format);
+
 
     }
 }
